61057df5bee9ac61243d154ca2d6f88b4b17d19c
Nicholas Bright <nikkistorme@gmail.com>
2019-03-20 08:18:01 -0500

    resetbutton,checkLetter,keyboardlistener,checkWin

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const qwerty = document.getElementById('qwerty'); //keyboard section
 const phrase = document.getElementById('phrase'); //phrase section
 const phraseUL = phrase.querySelector('ul'); // ul to hold li
 const startButton = document.querySelector('.btn__reset'); //start button
+const tries = document.getElementsByClassName('tries');
 const phrases = [ //create array of phrases
   'life before death',
   'strength before weakness',
@@ -15,11 +16,23 @@ const phrases = [ //create array of phrases
 let missed = 0; //number of incorrect guesses
 let letterFound = null; // null is default value for letter found by checkLetter
 
-// HIDE OVERLAY TO BEGIN GAME ON CLICK OF START BUTTON
+// HIDE OVERLAY TO BEGIN OR RESET GAME ON CLICK OF START BUTTON
 startButton.addEventListener('click', (e) => {
+  missed = 0;
+  for (i = 0; i < tries.length; i += 1) {
+    tries[i].firstChild.src = 'images/liveHeart.png';
+  }
+  const buttons = qwerty.getElementsByTagName('button');
+  for (i = 0; i < buttons.length; i += 1) {
+    buttons[i].className = '';
+    buttons[i].disabled = false;
+  }
   overlay.style.display = 'none';
+  phraseUL.innerHTML = '';
+  addPhraseToDisplay(getRandomPhraseAsArray(phrases));
 });
 
+// SEARCH THROUGH AN ARRAY FOR ITEM, RETURN TRUE OR FALSE
 function contains(src, test) {
   return src.indexOf(test) !== -1;
 }
@@ -52,18 +65,49 @@ function addPhraseToDisplay(arr) {
 
 // IF PRESSED LETTER IS IN PHRASE, SHOW THAT LETTER
 function checkLetter(keyPress) {
-  const lettersObject = document.getElementsByClassName('letter');
-  const lettersArray = Array.from(lettersObject);
-  const singleLetters = lettersArray.map(a => a.textContent);
-  for (let i = 0; i < singleLetters.length; i += 1) {
-    if (singleLetters[i] === keyPress) {
-      lettersArray[i].classList.add('show');
-      letterFound = singleLetters[i];
-    } else if (contains(singleLetters, keyPress) === false) {
-      letterFound = null;
+  const lettersObject = document.getElementsByClassName('letter'); //object of letters in phrase
+  const lettersArray = Array.from(lettersObject); //more specific object of letters
+  const singleLetters = lettersArray.map(a => a.textContent); //array of only letters
+  for (let i = 0; i < singleLetters.length; i += 1) { // loops through all letters in phrase
+    if (singleLetters[i] === keyPress) { // if pressed letter is on page,
+      lettersArray[i].classList.add('show'); // add class '.show'
+      letterFound = singleLetters[i]; // change letterFound to letter
+    } else if (contains(singleLetters, keyPress) === false) { // if pressed letter is NOT on page,
+      letterFound = null; // change letterFound to letter
     }
   }
-  return letterFound;
+  return letterFound; // return value of letterFound (found letter or null)
 }
 
-addPhraseToDisplay(getRandomPhraseAsArray(phrases));
+// WHAT HAPPENS WHEN LETTER IS CHOSEN TO BE GUESSED
+qwerty.addEventListener('click', (e) => { // listen for click
+  if (e.target.tagName === 'BUTTON') { // only button presses work
+    const button = e.target; // e.target = the pressed button
+    const keyPress = button.textContent; //keyPress is the letter inside the button element
+    button.className = 'chosen'; // add .chosen to button to display it has been used
+    button.disabled = true; // disable button so it cannt be used again
+    checkLetter(keyPress); // check if letter is in phrase
+    if (letterFound === null) { // letter not in phrase?
+      missed += 1; // update missed count
+      tries[tries.length - missed].firstChild.src = 'images/lostHeart.png'; // lose a heart
+    }
+    checkWin(); // check if game is over
+  }
+});
+
+// CHECK IF GAME IS OVER BY NUMBER OF CORRECT GUESSES VS NUMBER OF MISSES
+function checkWin() {
+  const shown = document.getElementsByClassName('show'); // all correctly guessed letters
+  const letters = document.getElementsByClassName('letter'); // all correct letters
+  function winOrLose(winlose, message) { // function to change overlay depending on win or lose
+    overlay.className = winlose; // change overlay to new version
+    overlay.querySelector('h2').textContent = message; // change overlay text to new version
+    startButton.textContent = 'Reset'; // change text of start button
+    overlay.style.display = 'flex'; // display new overlay
+  }
+  if (shown.length === letters.length) { // if all correct letters have been guessed,
+    winOrLose('win', 'CONGRATULATIONS!!! YOU WON!!!');
+  } else if (missed === 5) { // if game is not won, but all 5 misses are used
+    winOrLose('lose', 'YOU LOSE. BUMMER. TRY AGAIN?');
+  }
+}
